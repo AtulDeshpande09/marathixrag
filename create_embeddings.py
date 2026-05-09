a1e2f51b85e52852053777ffb134558a1f9b6fa9
AtulDeshpande09 <deshpandeatul05@gmail.com>
2026-05-09 19:21:55 +0000

    Added vectorDB

--- create_embeddings.py
+++ create_embeddings.py
@@ -34,14 +34,29 @@ print("complete!!!")
 client = chromadb.PersistentClient(path=db)
 collection = client.get_or_create_collection(name="my_chunks")
 
+BATCH_SIZE = 5000
+ids = [f"id_{i}" for i in range(len(texts))]
+
+
 print("")
 print("Adding embeddings to VectorDB...")
 
-collection.add(
-    ids=[f"id_{i}" for i in range(len(texts))],
-    embeddings=embeddings.tolist(),
-    documents=texts,
-    metadatas=metadatas
-)
+
+for i in range(0 , len(ids), BATCH_SIZE):
+    
+    batch_ids = ids[i : i + BATCH_SIZE]
+    batch_embeddings = embeddings[i : i+BATCH_SIZE].tolist()
+    batch_texts = texts[i : i + BATCH_SIZE]
+    batch_metadatas = metadatas[i : i + BATCH_SIZE]
+
+    collection.add(
+            ids=batch_ids,
+            embeddings=batch_embeddings,
+            documents=batch_texts,
+            metadatas=batch_metadatas
+            )
+
+    print(f"Uploaded batch {i//BATCH_SIZE + 1} ({len(batch_ids)} items)")
+
 
 print(f"Vector Database Saved at {db}")
